ee08af96ff0e3955bc346aa546702285c5fc9155
doosen11 <rosel032@d.umn.edu>
2018-04-05 02:36:45 -0500

    ....

--- TicTacToeClient/TicTacToeClient/Form1.cs
+++ TicTacToeClient/TicTacToeClient/Form1.cs
@@ -13,5 +13,72 @@ namespace TicTacToeClient {
         public Form1() {
             InitializeComponent();
         }
+
+        public bool game_over;
+        enum player { 
+            one,
+            two
+        };
+
+        public void Form1_Load(object sender, EventArgs e) {
+            textBox1.ReadOnly = true;
+            textBox1.Cursor = null;
+
+        }
+        private void button_handling(Button caller,String curr){
+
+            if (caller.Text != "") {
+                return;
+            }
+            if (game_over) {
+                return;
+            }
+            else if(){}
+
+        
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e) {
+
+        }
     }
 }
